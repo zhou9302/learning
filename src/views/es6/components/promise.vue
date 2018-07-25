b6c740eaeeb67b5451871ae530f86437c5204fbb
周颖 <zhouying@zhouyingdeMacBook-Pro.local>
2018-07-25 14:10:58 +0800

    promise

--- src/views/es6/components/promise.vue
+++ src/views/es6/components/promise.vue
@@ -7,6 +7,26 @@ export default{
     return {
 
     }
+  },
+  created () {
+    this.promise1()
+  },
+  methods: {
+    promise1 () {
+      let a = 3
+      const promise = new Promise(function (resolve, reject) {
+        if (a < 2) {
+          resolve('aa')
+        } else {
+          reject(new Error('错误必须用error对象抛出'))
+        }
+      })
+      promise.then((value) => {
+        console.log(value)
+      }, (error) => {
+        console.log(error)
+      })
+    }
   }
 }
 </script>
